092dbe67f41dfb38e233780abed30aed12b00134
Artini04 <89753669+Artini04@users.noreply.github.com>
2024-06-25 12:07:25 +0800

    update

--- script/index.js
+++ script/index.js
@@ -1,57 +1,29 @@
-"use strict"
-
-const baseWindChime = document.getElementById("wind-chime-base")
-const divWindChime = document.getElementById("wind-chime")
-const divWindChimeChildren = divWindChime.children
+const baseWindChime = document.getElementById("wind-chime-base");
+const divWindChime = document.getElementById("wind-chimes");
+const divWindChimeChildren = divWindChime.children;
 
 if (window.screen.availWidth > 480) {
-  tippy("#used-font", {
-    content: "Font used! Ebihara no kuseji TrueType",
-    placement: "right",
-    followCursor: true,
-    offset: [0, 20],
-    arrow: false,
-  })
+	tippy("#used-font", {
+		content: "Font used >> Ebihara no kuseji TrueType 🔗",
+		placement: "right",
+		followCursor: true,
+		offset: [0, 20],
+		arrow: false,
+	});
 
-  tippy("#github-link", {
-    content: "Github link!",
-    placement: "top",
-    followCursor: true,
-    offset: [0, 20],
-    arrow: false,
-  })
+	tippy("#github-link", {
+		content: "Github 🔗",
+		placement: "top",
+		followCursor: true,
+		offset: [0, 20],
+		arrow: false,
+	});
 
-  tippy("#info", {
-    content: "Aspiring WebDev | Studying Japanese",
-    placement: "left",
-    followCursor: true,
-    offset: [0, 20],
-    arrow: false,
-  })
-}
-
-appendAnimation()
-
-function appendAnimation() {
-  const cBaseDuration = parseInt(baseWindChime.getAttribute("anim-duration"))
-  baseWindChime.style.transformOrigin = "top"
-  baseWindChime.animate([{ rotate: "0deg" }, { rotate: "1deg" }], {
-    fill: "both",
-    duration: cBaseDuration,
-    iterations: Infinity,
-    easing: "ease-in-out",
-    direction: "alternate",
-  })
-
-  for (const chime of divWindChimeChildren) {
-    const cDuration = parseInt(chime.getAttribute("anim-duration"))
-    chime.style.transformOrigin = "top"
-    chime.animate([{ rotate: "-1deg" }, { rotate: "1deg" }], {
-      fill: "both",
-      duration: cDuration,
-      iterations: Infinity,
-      easing: "ease-in-out",
-      direction: "alternate",
-    })
-  }
+	tippy("#info", {
+		content: "Aspiring WebDev | Studying Japanese",
+		placement: "left",
+		followCursor: true,
+		offset: [0, 20],
+		arrow: false,
+	});
 }
