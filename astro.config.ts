4db105a65e6eced8d963359c398795a4edd50bee
Artini04 <89753669+Artini04@users.noreply.github.com>
2024-11-20 14:14:54 -0700

    Minor tweaks

--- astro.config.ts
+++ astro.config.ts
@@ -4,5 +4,14 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://artini04.github.io",
-  base: "/Artini04"
+  base: "/Artini04",
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern"
+        }
+      }
+    }
+  }
 });
