a97cd37f8e1d2f03196c556b2283fde24a1a033a
Artini04 <Artini04@proton.me>
2025-03-21 14:17:41 -0700

    update: full website redesign

--- astro.config.ts
+++ astro.config.ts
@@ -1,22 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 
-import icon from "astro-icon";
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://artini04.github.io",
   base: "/Artini04",
-
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: "modern"
-        }
-      }
-    }
-  },
 
   integrations: [icon()]
 })
